686ce6d00043c618e087cfbc5b91ddd6b599b4ee
linhey <is.linhey@outlook.com>
2026-01-12 11:13:47 +0800

    chore: 更新依赖版本并添加发布工作流

--- Package.swift
+++ Package.swift
@@ -8,13 +8,12 @@ let package = Package(
     platforms: [.macOS(.v13)],
     dependencies: [
         .package(url: "https://github.com/linhay/STColor.git", from: "1.0.0"),
-        .package(url: "https://github.com/linhay/STJSON.git", from: "1.1.4"),
-        .package(url: "https://github.com/swhitty/SwiftDraw", from: "0.18.1"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "5.1.3"),
+        .package(url: "https://github.com/linhay/STJSON.git", from: "1.3.2"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "6.2.0"),
         .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit", from: "0.2.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
-        .package(url: "https://github.com/linhay/swift-git", from: "2.0.1"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.8.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
+        .package(url: "https://github.com/linhay/swift-git", from: "2.0.3"),
         .package(url: "git@github.com:linhay/Stem.git", from: "2.1.0"),
         .package(url: "https://github.com/linhay/STFilePath", from: "1.2.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.5")
@@ -23,7 +22,7 @@ let package = Package(
         .target(name: "Features",
                 dependencies: [
                     "SwiftSoup",
-                    "SwiftDraw",
+                    .product(name: "Yams", package: "Yams"),
                     .product(name: "Logging", package: "swift-log"),
                     .product(name: "SwiftGit", package: "swift-git"),
                     .product(name: "Stem", package: "Stem"),
@@ -38,12 +37,12 @@ let package = Package(
                 "Features",
                 "SwiftSoup",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Yams", package: "Yams"),
             ]),
         .testTarget(
             name: "abrarionTests",
-            dependencies: ["abrarion",
-                           "Features",
-                          ]),
+            dependencies: [
+                "abrarion",
+                "Features"
+            ]),
     ]
 )
